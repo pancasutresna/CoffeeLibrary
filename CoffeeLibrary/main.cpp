43fe1f41bcf04ff275c660e1d8ab499815a75f8f
Panca Sutresna <panca.sutresna@gmail.com>
2016-10-27 16:09:02 +0700

    Initial commit with Assertion library

---
+++ CoffeeLibrary/main.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "co_assert.h"
+
+int main()
+{
+	ASSERT( 4 == 5 );
+	VERIFY( 4 == 5 );
+
+	return 0;
+}
---
+++ Test/main.cpp
@@ -0,0 +1,10 @@
+#include "../CoffeeLibrary/co_assert.h"
+
+int main()
+{
+	//ASSERT(4 == 5);
+	//VERIFY(4 == 5);
+	TRACE(L"1 + 2 = %d\n", 1 + 2);
+
+	return 0;
+}
